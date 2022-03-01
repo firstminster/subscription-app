42e8b1f9d221030cc7ba7a2a568f772030e48dca
Chinecherem Harry <chinecherempharry@gmail.com>
2022-03-02 00:42:45 +0100

    styling the the modal

---
+++ src/components/model/Model.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { Modal, Button, InputGroup, FormControl } from "react-bootstrap"
+
+
+const ModalComponent = () => {
+
+    const [show, setShow] = useState(false)
+
+    // handler
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
+    return (
+        <>
+            <Button onClick={handleShow}>
+                Signup
+            </Button>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header>
+                    <Modal.Title>
+                        Signup
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <InputGroup className='mb-3'>
+                        <InputGroup.Text>
+                            Email</InputGroup.Text>
+                        <FormControl type="email" />
+                    </InputGroup>
+                    <InputGroup className='mb-3'>
+                        <InputGroup.Text>
+                            Password</InputGroup.Text>
+                        <FormControl type="password" />
+                    </InputGroup>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant='secondary' onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant='primary'>
+                        Signup
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
+}
+
+export default ModalComponent
